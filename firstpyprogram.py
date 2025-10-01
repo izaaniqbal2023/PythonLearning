a9b9ad1a1de3a69576bf4fdafbde9cb92b810da0
izaaniqbal2023 <izaan.iqbal.2023@gmail.com>
2025-09-30 21:44:00 -0500

    Update program
    even better

--- firstpyprogram.py
+++ firstpyprogram.py
@@ -1,7 +1,15 @@
 # Izaan you can write your python code and check-in
-order = input("what is your order? ")
-print("this is the order: " + order)
-order = input("Please change order: ")
-print("New order: "+order)
+order = input("What is your order? ")
+print("This is the order: " + order)
+print("That is not a valid order,\nplease change it into the following categores")
+print("\n:burger\n:pizza\n:or butta chikan")
+order = int(input("Change order: "))
+if order = "burger"
+print("Cost:"+cost+" dollors")
+print("Have a nice day")
+elif order = "pizza"
+print("Cost:20 dollors")
+order = "butta chikan"
+    
 cost = "10"
 print("Cost " +cost+" dollors")
