0a1cf22fffa57218371d9ccceac395c19b12af66
izaaniqbal2023 <izaan.iqbal.2023@gmail.com>
2025-09-30 21:26:16 -0500

    MemorizedCode.py
    This code i wrote from my memory without any assistance.

---
+++ firstpyprogram.py
@@ -0,0 +1,7 @@
+# Izaan you can write your python code and check-in
+order = input("what is your order? ")
+print("this is the order: " + order)
+order = input("Please change order: ")
+print("New order: "+order)
+cost = "10"
+print("Cost " +cost+" dollors")
